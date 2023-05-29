807e060319676ce50577268e7d016f5a4c0c719f
joshuagao114 <joshua.gao114@gmail.com>
2023-05-29 15:43:44 +0800

    TEST

--- Assets/_Project/Scripts/TestScript.cs
+++ Assets/_Project/Scripts/TestScript.cs
@@ -6,11 +6,13 @@ public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {    
+    {
+        Debug.Log("TEST");
     }
 
     // Update is called once per frame
     void Update()
-    {  
+    {
+        Debug.Log("TEST2");
     }
 }
