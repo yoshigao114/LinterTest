829b528a20414c70b7469e4aa07edd6472b52a49
joshuagao114 <joshua.gao114@gmail.com>
2023-05-29 15:55:28 +0800

    TEst

--- Assets/_Project/Scripts/TestScript.cs
+++ Assets/_Project/Scripts/TestScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    string TEst;
+    private string TEst;
     // Start is called before the first frame update
     void Start()
     {
