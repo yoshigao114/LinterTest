05fc7f62fb73be46062b8984d07248f5da8ea6a6
joshuagao114 <joshua.gao114@gmail.com>
2023-06-08 14:56:07 +0800

    test

--- Assets/_Project/Scripts/Player.cs
+++ Assets/_Project/Scripts/Player.cs
@@ -7,7 +7,6 @@ namespace InfallibleCode
         private Sheep _sheep;
         private string test;
         private string test2;
-        private const string test12345 = "Test";
         private void Awake()
         {
             _sheep = GetComponent<Sheep>();
